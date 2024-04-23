6199c7caf09b75a961e3f003a14e92624880002c
Atul Ranjan <atulranjan789@gmail.com>
2024-04-24 01:14:15 +0530

    remove

--- routes/routes.go
+++ routes/routes.go
@@ -5,8 +5,6 @@ import (
 
 	"github.com/atul-007/leaderboard/controllers"
 	"github.com/gin-gonic/gin"
-	swaggerFiles "github.com/swaggo/files"
-	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 // SetupRouter sets up the routes for the applicationS
@@ -20,9 +18,7 @@ func SetupRouter() *gin.Engine {
 	r.POST("/submit", highScoreController.SubmitScore)
 	r.GET("/get_rank", highScoreController.GetRank)
 	r.GET("/list_top_n", highScoreController.ListTopN)
-	// r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	// r.GET("/swagger.json", ginSwagger.WrapHandler(swaggerFiles.Handler))
-	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
 	r.GET("/swagger.json", func(c *gin.Context) {
 		http.ServeFile(c.Writer, c.Request, "docs/swagger.yaml") // Adjust the path to your swagger.yaml file
 	})
